f31517e5681c964150b90b2e7ab3d2bedd9f4855
Jose Mujica <jomuji@gmail.com>
2016-08-25 18:09:17 -0400

    test ng-bootstrap
    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from './carousel';
 import { ContentComponent } from './content';
+import { AlertComponent } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  directives: [CarouselComponent, ContentComponent]
+  //templateUrl: 'app.component.html',
+  template: '<alert type="info">ng2-bootstrap hello world!</alert>',
+  //directives: [CarouselComponent, ContentComponent]
+  directives: [AlertComponent]
 })
 export class AppComponent {
   title = 'app works!';
